694fe1db63b8459a1dac4826d0a35b658359d03d
onetwo7 <lahoohal@gmail.com>
2018-02-09 00:57:55 +0300

    google oauth flow

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+require('./models/User');
+
+mongoose.connect(keys.db.mongoURI).catch(err => {
+  console.log('encountered error when connection to DB -', err);
 });
+
+require('./services/passport');
+
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookie.secret]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 app.listen(PORT);
---
+++ test.js
@@ -0,0 +1,4 @@
+const credentials = require('./config/keys');
+
+console.log(credentials.google.client_id);
+console.log(credentials.google.client_secret);
